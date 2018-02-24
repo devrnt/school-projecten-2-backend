e1e495f817a71f4ed9e8025567e4ed855870ceb3
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-02-24 22:53:24 +0100

    Aanpassen PadSessionFilter: pad uit DB halen

--- g16_dotnet/Filters/PadSessionFilter.cs
+++ g16_dotnet/Filters/PadSessionFilter.cs
@@ -4,46 +4,45 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 
-namespace g16_dotnet.Filters
-{
-    public class PadSessionFilter : ActionFilterAttribute
-    {
+namespace g16_dotnet.Filters {
+    public class PadSessionFilter : ActionFilterAttribute {
         private Pad _pad;
-       
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
+        private IPadRepository _padRepository;
+
+        public PadSessionFilter(IPadRepository padRepository) {
+            _padRepository = padRepository;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context) {
             _pad = ReadPadFromSession(context.HttpContext);
             context.ActionArguments["pad"] = _pad;
             base.OnActionExecuting(context);
         }
 
-        public override void OnActionExecuted(ActionExecutedContext context)
-        {
+        public override void OnActionExecuted(ActionExecutedContext context) {
             WriteCartToSession(_pad, context.HttpContext);
             base.OnActionExecuted(context);
         }
 
-        private Pad ReadPadFromSession(HttpContext context)
-        {
+        private Pad ReadPadFromSession(HttpContext context) {
             Pad pad = null;
-            if (context.Session.GetString("pad") == null)
-            {
-                Oefening oefening = new Oefening("Opgave 1", "abc");
-                GroepsBewerking groepsBewerking = new GroepsBewerking("def");
-                string toegangsCode = "xyz";
-                Opdracht testOpdracht = new Opdracht(toegangsCode, oefening, groepsBewerking) { VolgNr = 1 };
-                Opdracht testOpdracht2 = new Opdracht(toegangsCode, oefening, groepsBewerking) { VolgNr = 2 };
-                Actie testActie = new Actie("Ga naar afhaalchinees", testOpdracht);
-                pad = new Pad(new List<Opdracht> { testOpdracht, testOpdracht2 }, new List<Actie> { testActie }) { PadId = 1 }; 
-            } else
-            {
+            if (context.Session.GetString("pad") == null) {
+                var padId = 1;
+                pad = _padRepository.GetById(padId);
+
+                //Oefening oefening = new Oefening("Opgave 1", "abc");
+                //GroepsBewerking groepsBewerking = new GroepsBewerking("def");
+                //string toegangsCode = "xyz";
+                //Opdracht testOpdracht = new Opdracht(toegangsCode, oefening, groepsBewerking) { VolgNr = 1 };
+                //Opdracht testOpdracht2 = new Opdracht(toegangsCode, oefening, groepsBewerking) { VolgNr = 2 };
+                //Actie testActie = new Actie("Ga naar afhaalchinees", testOpdracht);
+                //pad = new Pad(new List<Opdracht> { testOpdracht, testOpdracht2 }, new List<Actie> { testActie }) { PadId = 1 };
+            } else {
                 pad = JsonConvert.DeserializeObject<Pad>(context.Session.GetString("pad"));
             }
             return pad;
         }
 
-        private void WriteCartToSession(Pad pad, HttpContext context)
-        {
+        private void WriteCartToSession(Pad pad, HttpContext context) {
             context.Session.SetString("pad", JsonConvert.SerializeObject(pad));
         }
     }
