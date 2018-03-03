67330deaae3e1ab582d58570d7b866d5e35ef5fb
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-03 13:30:25 +0100

    Toevoegen huisstijl CSS

---
+++ g16_dotnet/gulpfile.js
@@ -0,0 +1,11 @@
+﻿const gulp = require('gulp'),
+    fs = require('fs'),
+    sass = require('gulp-sass');
+
+// other content removed
+
+gulp.task('sass', function () {
+    return gulp.src('Styles/main.scss')
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest('wwwroot/css'));
+});
---
+++ g16_dotnet/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿window.onload = () => {
+    let spans = document.querySelectorAll('.close');
+    console.log(spans);
+    spans.forEach(span => span.addEventListener('click', event => {
+        closeAlert(event.target.parentNode);
+    }));
+
+    function closeAlert(node) {
+        node.style.display = 'none';
+    }
+
+}
