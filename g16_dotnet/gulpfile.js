8fd1d445b7d324cebce7edb354b9c2ff08a91673
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-04 16:59:02 +0100

    Aangepast CSS SessieDetail, BeheerSessies

--- g16_dotnet/gulpfile.js
+++ g16_dotnet/gulpfile.js
@@ -2,10 +2,13 @@
     fs = require('fs'),
     sass = require('gulp-sass');
 
-// other content removed
-
 gulp.task('sass', function () {
     return gulp.src('Styles/main.scss')
         .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(gulp.dest('wwwroot/css'));
+});
+
+// onchange exec task 'sass'
+gulp.task('sass:watch', function () {
+    gulp.watch('Styles/*.scss', ['sass'])
 });
