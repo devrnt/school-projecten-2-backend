dc1fb8f57932079a0e2f81806a5b8646d2e7a4cd
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-19 19:53:58 +0100

    Fix issue in SessieRepository: bij ingeven van een sessiecode die niet geldig is null opvangen

--- g16_dotnet/Data/Repositories/SessieRepository.cs
+++ g16_dotnet/Data/Repositories/SessieRepository.cs
@@ -3,41 +3,34 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace g16_dotnet.Data.Repositories
-{
-    public class SessieRepository : ISessieRepository
-    {
+namespace g16_dotnet.Data.Repositories {
+    public class SessieRepository : ISessieRepository {
 
         private readonly ApplicationDbContext _context;
         private readonly DbSet<Sessie> _sessies;
 
-        public SessieRepository(ApplicationDbContext context)
-        {
+        public SessieRepository(ApplicationDbContext context) {
             _context = context;
             _sessies = context.Sessies;
         }
 
-        public void Add(Sessie sessie)
-        {
+        public void Add(Sessie sessie) {
             _sessies.Add(sessie);
         }
 
-        public void Delete(Sessie sessie)
-        {
+        public void Delete(Sessie sessie) {
             _sessies.Remove(sessie);
         }
 
-        public IEnumerable<Sessie> GetAll()
-        {
+        public IEnumerable<Sessie> GetAll() {
             return _sessies.Include(s => s.Groepen)
                 .ThenInclude(g => g.Leerlingen)
                 .Include(s => s.Klas)
                 .ToList();
         }
 
-        public Sessie GetById(int id)
-        {
-            var sessie =  _sessies
+        public Sessie GetById(int id) {
+            var sessie = _sessies
                 .Include(s => s.Klas)
                 .Include(s => s.Groepen)
                     .ThenInclude(g => g.Leerlingen)
@@ -50,34 +43,31 @@ namespace g16_dotnet.Data.Repositories
                         .ThenInclude(p => p.Acties)
                             .ThenInclude(pa => pa.Actie)
                 .SingleOrDefault(s => s.SessieCode == id);
-
-            foreach(var groep in sessie.Groepen)
-            {
-                switch (groep.Pad.State)
-                {
-                    case States.Geblokkeerd:
-                        groep.Pad.PadState = new GeblokkeerdPadState("Geblokkeerd");
-                        break;
-                    case States.Opdracht:
-                        groep.Pad.PadState = new OpdrachtPadState("Opdracht");
-                        break;
-                    case States.Actie:
-                        groep.Pad.PadState = new ActiePadState("Actie");
-                        break;
-                    case States.Vergrendeld:
-                        groep.Pad.PadState = new VergrendeldPadState("Vergrendeld");
-                        break;
-                    case States.Schatkist:
-                        groep.Pad.PadState = new SchatkistPadState("Geblokkeerd");
-                        break;
+            if (sessie != null) {
+                foreach (var groep in sessie.Groepen) {
+                    switch (groep.Pad.State) {
+                        case States.Geblokkeerd:
+                            groep.Pad.PadState = new GeblokkeerdPadState("Geblokkeerd");
+                            break;
+                        case States.Opdracht:
+                            groep.Pad.PadState = new OpdrachtPadState("Opdracht");
+                            break;
+                        case States.Actie:
+                            groep.Pad.PadState = new ActiePadState("Actie");
+                            break;
+                        case States.Vergrendeld:
+                            groep.Pad.PadState = new VergrendeldPadState("Vergrendeld");
+                            break;
+                        case States.Schatkist:
+                            groep.Pad.PadState = new SchatkistPadState("Geblokkeerd");
+                            break;
+                    }
                 }
             }
-
             return sessie;
         }
 
-        public void SaveChanges()
-        {
+        public void SaveChanges() {
             _context.SaveChanges();
         }
     }
