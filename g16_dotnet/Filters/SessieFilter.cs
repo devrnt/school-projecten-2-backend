87d62522a6ff32d69d0988e61404c986f5aae0c0
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-03-21 15:06:17 +0100

    Fix SessieFilter danku Sam

--- g16_dotnet/Filters/SessieFilter.cs
+++ g16_dotnet/Filters/SessieFilter.cs
@@ -20,31 +20,26 @@ namespace g16_dotnet.Filters
         }
 
         public override void OnActionExecuting(ActionExecutingContext context)
-        {
-        
-
-            string sessieCode = (context.Controller as Controller).ViewData["sessieCode"] as string;
-            string doelgroep = (context.Controller as Controller).ViewData["Doelgroep"] as string;
-            if (sessieCode != null)
-            {
-                context.HttpContext.Session.SetString("sessieCode", sessieCode);
-                context.HttpContext.Session.SetString("Doelgroep", doelgroep);
-                
-            }
-
+        {    
             if (context.HttpContext.Session.GetString("sessieCode")!=null)
             {
                   _sessie = _sessieRepository.GetById(System.Int32.Parse(context.HttpContext.Session.GetString("sessieCode")));
             context.ActionArguments["sessie"] = _sessie; 
             }
-         
-
 
             base.OnActionExecuting(context);
         }
 
         public override void OnActionExecuted(ActionExecutedContext context)
         {
+            string sessieCode = (context.Controller as Controller).ViewData["sessieCode"] as string;
+            string doelgroep = (context.Controller as Controller).ViewData["Doelgroep"] as string;
+            if (sessieCode != null) {
+                context.HttpContext.Session.SetString("sessieCode", sessieCode);
+                context.HttpContext.Session.SetString("Doelgroep", doelgroep);
+
+            }
+
             _sessieRepository.SaveChanges();
             base.OnActionExecuted(context);
         }
