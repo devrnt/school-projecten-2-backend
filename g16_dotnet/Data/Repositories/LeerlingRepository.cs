1755cf41054c4cbf87b18c8398b5dd28df409a68
Sam Dhondt <sam.dhondt.w1599@student.hogent.be>
2018-03-22 12:52:30 +0100

    Add ignored files

---
+++ g16_dotnet/Data/Repositories/LeerlingRepository.cs
@@ -0,0 +1,37 @@
+﻿using g16_dotnet.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace g16_dotnet.Data.Repositories
+{
+    public class LeerlingRepository : ILeerlingRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Leerling> _leerlingen;
+
+        public LeerlingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _leerlingen = _context.Leerlingen;
+        }
+
+        public IEnumerable<Leerling> GetAll()
+        {
+            return _leerlingen.ToList();
+        }
+
+        public Leerling GetByEmail(string email)
+        {
+            return _leerlingen.SingleOrDefault(l => l.Email == email);
+        }
+
+        public Leerling GetById(int id)
+        {
+            return _leerlingen.SingleOrDefault(l => l.LeerlingId == id);
+
+        }
+    }
+}
---
+++ g16_dotnet/Models/Domain/ILeerlingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace g16_dotnet.Models.Domain
+{
+    public interface ILeerlingRepository
+    {
+        Leerling GetById(int id);
+
+        IEnumerable<Leerling> GetAll();
+
+        Leerling GetByEmail(string email);
+    }
+}
