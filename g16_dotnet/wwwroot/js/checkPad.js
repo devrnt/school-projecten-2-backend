0b2374c6789b38825a9f1138c2562f5dfff9c775
Sam Dhondt <sam.dhondt.w1599@student.hogent.be>
2018-03-25 17:15:08 +0200

    Aanpassen teksten Script voor checkPad toegevoegd

---
+++ g16_dotnet/wwwroot/js/checkPad.js
@@ -0,0 +1,34 @@
+﻿var checkPadModule = (function () {
+    var padId;
+
+    function init() {
+        padId = window.localStorage.getItem("padId");
+        var checkPad = function () {
+            console.log("Checking pad...");
+            $.ajax({
+                type: "GET",
+                url: "/Spel/CheckPad",
+                data: { "padId": parseInt(padId) },
+                contentType: "application/json;",
+                dataType: "json",
+                success: function (response) {
+                    if (!response.isGeblokkeerd && !response.isVergrendeld) {
+                        $("#verdergaan").removeClass("not-active");
+                    } else {
+                        setTimeout(checkPad, 3000);
+                    }
+                }
+            });
+        }
+
+        checkPad();
+    }
+
+    return {
+        init: init
+    }
+}());
+
+$(document).ready(function () {
+    checkPadModule.init();
+});
